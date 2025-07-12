c146c0b610b3828482df5483cb82c78a8788bd55
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-07-12 13:21:43 -0300

    build: create initial project  - using jpa, spring security, spring web, dev tools, lombok, h2 database and java jwt

---
+++ src/test/java/com/lucas/login_api/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.login_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
