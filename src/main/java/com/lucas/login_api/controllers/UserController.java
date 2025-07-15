0b22aceb2d7c24c43fa37a4fb6c461ba5640065e
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-07-14 22:19:27 -0300

    feat: update endpoint /user to return all user data: id, name and email

--- src/main/java/com/lucas/login_api/controllers/UserController.java
+++ src/main/java/com/lucas/login_api/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.lucas.login_api.controllers;
 
+import com.lucas.login_api.domain.user.User;
+import com.lucas.login_api.dto.UserResponseDTO;
 import com.lucas.login_api.infra.security.CustomUserDetailService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("Sucesso!");
+    public ResponseEntity<UserResponseDTO> getUser(@AuthenticationPrincipal User user) {
+        UserResponseDTO response = new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/lucas/login_api/dto/UserResponseDTO.java
@@ -0,0 +1,8 @@
+package com.lucas.login_api.dto;
+
+public record UserResponseDTO(
+        String id,
+        String name,
+        String email
+){
+}
